3baef1449b6f9b8fa14d6be0daba50146c7e4ec4
Jordan-type <jordanmuthemba25@gmail.com>
2021-08-16 22:23:43 +0300

    complete 😊😊

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,135 @@
 import React, { Component } from 'react';
-// import logo from '../logo-eth.png';
+import Web3 from 'web3'
+import Token from '../abis/Token.json'
+import EthSwap from '../abis/EthSwap.json'
+import Navbar from './Navbar'
+import Main from './Main'
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+// load the blockchain
+async loadBlockchainData() {
+  const web3 = window.web3
+ 
+  // get account
+  const accounts = await web3.eth.getAccounts()
+  console.log("my accounts", accounts)
+  this.setState({ account: accounts[0] })
+
+  // get eth balance
+  const ethBalance = await web3.eth.getBalance(this.state.account)
+  console.log("my eth balance", ethBalance)
+  this.setState({ ethBalance })
+
+  // load token data
+  const networkId = await web3.eth.net.getId()
+  console.log("ethereum network Id:", networkId)
+  const tokenData = Token.networks[networkId]
+  console.log("Token Data:", tokenData)
+
+  if(tokenData) {
+    const token = new web3.eth.Contract(Token.abi, tokenData.address)
+    console.log("Token Abi:", token)
+    this.setState({ token })
+    //get token balance
+    let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+    this.setState({ tokenBalance: tokenBalance.toString() })
+  } else {
+    window.alert('Token contract not deployed to detected network.')
+  }
+  
+  // load EthSwap data
+  const ethSwapData = EthSwap.networks[networkId]
+    if(ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+      console.log("EthSwap Abi:", ethSwapData.address)
+      this.setState({ ethSwap })
+    } else {
+      window.alert('EthSwap contract not deployed to detected network.')
+  }
+
+  this.setState({ loading: false })
+}
+
+// load web3 to connect with metamask
+   async loadWeb3() {
+     if (window.ethereum) {
+       window.web3 = new Web3(window.ethereum)
+       await window.ethereum.enable()
+     } else if (window.web3) {
+       window.web3 = new Web3(window.web3.currentProvider)
+     } else {
+       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+     }
+   }
+
+  //  buy tokens with eth function
+  buyTokens = (etherAmount) => {
+    this.setState({ loading: true })
+    this.state.ethSwap.methods.buyTokens().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  //  sell tokens for eth function
+  sellTokens = (tokenAmount) => {
+    this.setState({ loading: true })
+    this.state.token.methods.approve(this.state.ethSwap.address, tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.state.ethSwap.methods.sellTokens(tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+
+
+   constructor(prop) {
+     super(prop)
+     this.state = {
+       account: '',
+       token: {},
+       ethSwap: {},
+       ethBalance: '0',
+       tokenBalance: '0',
+       loading: true
+     }
+   }
+
+
+
   render() {
+    // lazy loading
+    let content 
+    if(this.state.loading) {
+      content = <p id='loader' className="text-center">Loading...</p>
+    } else {
+      content = <Main
+          ethBalance={this.state.ethBalance}
+          tokenBalance={this.state.tokenBalance}
+          buyTokens={this.buyTokens}
+          sellTokens={this.sellTokens}
+         />
+    }
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="https://thelightideas.co.ke/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Light Ideas
-          </a>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
                 <a
                   href="https://thelightideas.co.ke/"
                   target="_blank"
                   rel="noopener noreferrer"
-                >
-                  {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                  >
                 </a>
-                <h1>Light Ideas Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
+                {content}
               </div>
             </main>
           </div>
