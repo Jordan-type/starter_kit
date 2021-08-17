2c405e3fd285058c4c95ce3340243ddab714d66d
Jordan-type <jordanmuthemba25@gmail.com>
2021-08-17 19:47:29 +0300

    "deployed to kovan net😊😊😊🎉🎉"

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,28 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1",  // Localhost (default: none)
+      port: 7545,         // Standard Ethereum port (default: none)
+      network_id: "*"     // Any network (default: none)
     },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','),  //  private key
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` //  url to an ethereum node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
@@ -21,3 +36,5 @@ module.exports = {
     }
   }
 }
+
+
